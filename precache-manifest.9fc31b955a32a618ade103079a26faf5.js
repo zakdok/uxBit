b24703a602d6184a6f4339634308978c9bb66be1
zakdok <zakdok@naver.com>
2020-07-15 21:22:55 +0900

    Updates

---
+++ precache-manifest.9fc31b955a32a618ade103079a26faf5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef515d5354429c550257b3d2e907467",
+    "url": "/uxBit/index.html"
+  },
+  {
+    "revision": "3e7a472110a7faa7bc11",
+    "url": "/uxBit/static/css/2.df79aca5.chunk.css"
+  },
+  {
+    "revision": "a2cdf805d687066e4567",
+    "url": "/uxBit/static/css/main.1c61d2cc.chunk.css"
+  },
+  {
+    "revision": "3e7a472110a7faa7bc11",
+    "url": "/uxBit/static/js/2.a961087e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/uxBit/static/js/2.a961087e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2cdf805d687066e4567",
+    "url": "/uxBit/static/js/main.4bfdac48.chunk.js"
+  },
+  {
+    "revision": "c9b8b5b7ea75f69ddd2b",
+    "url": "/uxBit/static/js/runtime-main.03db1b8c.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/uxBit/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/uxBit/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/uxBit/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/uxBit/static/media/slick.f97e3bbf.svg"
+  }
+]);
